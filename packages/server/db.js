95caf899e5ada77ef38174bd9a3402df0b2b2837
YAKUB_YO <yakuboyusuf@gmail.com>
2023-09-17 08:21:35 +0100

    other files

---
+++ packages/server/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      database: process.env.POSTGRES_DB,
+      host: process.env.DATABASE_HOST,
+      password: process.env.POSTGRES_PASSWORD,
+      user: process.env.POSTGRES_USER,
+      port: process.env.DATABASE_PORT,
+    });
+
+module.exports = pool;

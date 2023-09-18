c2676acb8e170e79ef22928dad220a222242c277
YAKUB_YO <yakuboyusuf@gmail.com>
2023-09-18 10:34:22 +0100

    Successfully run the app after modifying the /server/db.js for ssl

--- packages/server/db.js
+++ packages/server/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = process.env.DATABASE_URL
   ? new Pool({
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
+      ssl: false,
     })
   : new Pool({
       database: process.env.POSTGRES_DB,
